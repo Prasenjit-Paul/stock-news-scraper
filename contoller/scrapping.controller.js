e30afe57afeb0f1f7bd9c687c48ab737e49d302c
Prasenjit Paul <pprasenjit00@gmail.com>
2024-10-24 23:14:08 +0530

    day 1

--- contoller/scrapping.controller.js
+++ contoller/scrapping.controller.js
@@ -4,12 +4,29 @@ const cheerio = require('cheerio');
 exports.scrap = async (req, res) => {
     const scrapedData = await getHtml('https://www.moneycontrol.com/news/business/earnings/itc-q2fy25-results-net-profit-jumps-3-1-to-rs-5078-3-crore-revenue-rises-16-12849642.html');
 
+    let result = {};
+
     const $ = cheerio.load(scrapedData);
+
+    let keyWords = $('meta[name="Keywords"]').attr('content');
+    console.log(keyWords.includes('itc cigarette price'));
+
+    let heading = $('.article_title').text();
+
+    let allPTags = [];
+
+    $('.content_wrapper').each((index, data) => {
+        allPTags.push($(data).find('p').text() + "~");
+    });
+
+    result.heading = heading;
+    result.keyWords = keyWords;
+    result.content = allPTags;
 
     res.json({
         status: 'SUCCESS',
-        message: "Scraping process started",
-        data: $,
+        message: "Scraping Successfully",
+        data: result,
         responseCode: 200
     })
 }
