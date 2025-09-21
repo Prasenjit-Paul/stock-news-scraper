5761a43db66a1a22df6e97487f1c19539cf55dc7
Prasenjit Paul <pprasenjit00@gmail.com>
2025-09-22 01:23:22 +0530

    added again avoid list

--- utils/checkKeywords.util.js
+++ utils/checkKeywords.util.js
@@ -31,10 +31,10 @@ exports.checkKeyword = (keyword, title) => {
         "earnings", "deal", "order", "industry", "shares",
         "indian Stock market", "Dividend issue", "Result news", "tariff", "FOMC", "Bond", "dollar"
     ];
-    // const avoidedList = ["recommendation", "recommend", "recommends", "advice", "advices"];
+    const avoidedList = ["recommendation", "recommend", "recommends", "advice", "advices"];
 
     const localDataSet = target.map(item => item.toUpperCase());
-    // const localAvoidSet = avoidedList.map(item => item.toUpperCase());
+    const localAvoidSet = avoidedList.map(item => item.toUpperCase());
 
     let keywordMatch = false, titleMatch = false;
 
@@ -43,7 +43,7 @@ exports.checkKeyword = (keyword, title) => {
         const keywordUpper = keyword.map(k => k.toUpperCase());
 
         // Avoid list check
-        // if (keywordUpper.some(k => localAvoidSet.includes(k))) return false;
+        if (keywordUpper.some(k => localAvoidSet.includes(k))) return false;
 
         keywordMatch = keywordUpper.some(k => localDataSet.includes(k));
     }
@@ -53,7 +53,7 @@ exports.checkKeyword = (keyword, title) => {
         // Normalize: remove punctuation, split on spaces
         const arrayFromString = title.replace(/[^\w\s]/g, "").split(/\s+/).map(t => t.toUpperCase());
 
-        // if (arrayFromString.some(t => localAvoidSet.includes(t))) return false;
+        if (arrayFromString.some(t => localAvoidSet.includes(t))) return false;
 
         titleMatch = arrayFromString.some(t => localDataSet.includes(t));
     }
